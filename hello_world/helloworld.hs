101302fe2957a799178fcced333307e792f3ca62
Ben Kolera <ben.kolera@gmail.com>
2012-05-24 20:28:42 +1000

    Initial commit.

---
+++ hello_world/helloworld.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,
+             TemplateHaskell, OverloadedStrings #-}
+import Yesod
+import Data.Text
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/hello/#Text HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Text -> Handler RepHtml
+getHomeR name = defaultLayout [whamlet|Hello #{name}!|]
+
+main :: IO ()
+main = warpDebug 3000 HelloWorld
